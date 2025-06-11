61870a1f621d71920d95fea1c42b5437589e776a
charleslando <clando1@mail.yu.edu>
2025-06-11 09:31:52 -0400

    modifief EclipseTrades.sql

--- Shayne/EclipseTrades.sql
+++ Shayne/EclipseTrades.sql
@@ -9,8 +9,3 @@ WHERE commission > 0
   AND broker = 'Madison Brittner'
 
 GROUP BY client, trader, broker
-
-
--- select trade_date
--- from trades
--- where trade_date between '04/01/2023' AND '05/01/2023'
